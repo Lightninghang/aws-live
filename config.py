c7220e83f613e8292ccaa987fb518c91964e3089
Lightninghang <chenyuhang2150@gmail.com>
2026-08-03 13:57:49 +0800

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ customhost = "employee.caymopgxe74d.us-east-1.rds.amazonaws.com"
 customuser = "aws_user"
 custompass = "Bait3273"
 customdb = "employee"
-custombucket = "chenyuhang-employee1"
+custombucket = "chenyuhang-employee"
 customregion = "us-east-1"
 
